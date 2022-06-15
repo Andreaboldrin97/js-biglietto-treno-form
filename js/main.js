8a12bdff97812320e1d2369f4a977ec83a64ae55
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-15 16:17:40 +0200

    added ticket data (name , price) in html

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 //creazione var legate all'user
 
+
 //creo una var per chiedere il nome all'utente
 let userName ;
 //cero una var per chiedere l'età dell'utente
 let userAge ;
 //creo una var per chiedere quanti km dovrà percorrere
 let kmToCover ;
+//creo una var per il numero di carozza
+let carriageNumber ;
 
 //creo una var per chiamare il btn
 let btnGenerated = document.getElementById('btn-generated');
+
+
+
 //?Compilazione form 
 
 btnGenerated.addEventListener(`click`, 
@@ -17,38 +23,43 @@ function(){
     let userName = document.getElementById(`user_name`).value ;
     console.log(userName)
     //cero una var per chiedere l'età dell'utente
-    let userAge = parseInt( document.getElementById('user_age').value );
+    let userAge = document.getElementById('user_age').value ;
     console.log(userAge)
     //creo una var per chiedere quanti km dovrà percorrere
     let kmToCover = parseInt(document.getElementById('km_tocover').value);
     console.log(kmToCover)
 
-    //! creazione costo biglietto
 
-let PriceNoDiscount = (kmToCover * 0.26);
-console.log(PriceNoDiscount);
+        //! creazione costo biglietto
 
-let PriceDiscount ;
-let Price ;
+    let PriceNoDiscount = (kmToCover * 0.26);
+    console.log(PriceNoDiscount);
 
-if (isNaN(userAge) == true){
-    alert(`l'età da lei indicata non è valida`);
-} else if (isNaN(kmToCover) == true){
-    alert(`i km da lei indicati non sono validi`);
- } else {
-          if( userAge < 18){
-              PriceDiscount=((PriceNoDiscount * 15 ) / 100);
-          Price=( PriceNoDiscount - PriceDiscount ).toFixed(2);
-     }else if(userAge > 65){
-              PriceDiscount=((PriceNoDiscount * 35 ) / 100);
-          Price=( PriceNoDiscount - PriceDiscount ).toFixed(2);
-     }else{
-              Price=PriceNoDiscount.toFixed(2);
-     }
- }
+    let PriceDiscount ;
+    let Price ;
 
- console.log(PriceDiscount);
- console.log(Price + '€');
+    if (isNaN(kmToCover) == true){
+        alert(`i km da lei indicati non sono validi`);
+    } else {
+            if( userAge == `age_<18`){
+                PriceDiscount=((PriceNoDiscount * 15 ) / 100);
+            Price=( PriceNoDiscount - PriceDiscount ).toFixed(2);
+        }else if(userAge == `age_>65`){
+                PriceDiscount=((PriceNoDiscount * 35 ) / 100);
+            Price=( PriceNoDiscount - PriceDiscount ).toFixed(2);
+        }else{
+                Price=PriceNoDiscount.toFixed(2);
+        }
+    }
+
+       
+    console.log(PriceDiscount);
+    console.log(Price + '€');
+
+    //! creazione biglietto
+    
+    document.getElementById('name').innerHTML += userName;
+    document.getElementById('price').innerHTML += Price + `€`;
 }
 )
 
